332d33c9b268b565b87bcaa99cf840803f65aa80
Lina Hu <lina.l.hu@gmail.com>
2022-05-26 17:30:34 -0400

    reorganized file structure

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,9 @@
 //Redux data store
 
 import { configureStore } from "@reduxjs/toolkit";
-import coordinatesReducer from "../reducers/coordinatesReducer";
-import weatherReducer from "../reducers/weatherReducer";
-import cityReducer from "../reducers/cityReducer"
+import coordinatesReducer from "./reducers/coordinatesReducer";
+import weatherReducer from "./reducers/weatherReducer";
+import cityReducer from "./reducers/cityReducer"
 
 const store = configureStore({
     reducer: {  coordinates: coordinatesReducer, 
